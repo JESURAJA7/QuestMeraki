cf1957e498fa4ac1a7fc88648e952f494f8065e9
JESURAJA7 <105117365+JESURAJA7@users.noreply.github.com>
2025-06-11 17:18:27 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import FeaturedPost from '../components/FeaturedPost';
 
 const API_URL = import.meta.env.VITE_API_URL;
-//console.log('API_URL:', API_URL);
+console.log('API_URL:', API_URL);
 type Post = {
   _id: string;
   title: string;
