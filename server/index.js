35197b76f9de233d00bc3874e3385ac667545dba
JESURAJA7 <105117365+JESURAJA7@users.noreply.github.com>
2025-06-11 17:06:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['https://questmeraki.netlify.app', 'http://localhost:5173/'],
+  origin: ['https://questmeraki1.netlify.app', 'http://localhost:5173/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
