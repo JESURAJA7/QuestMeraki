b49487bffa38da41052992085e8422b11f451f00
JESURAJA7 <105117365+JESURAJA7@users.noreply.github.com>
2025-07-04 13:53:39 +0530

    Update BlogsPage.tsx

--- src/pages/BlogsPage.tsx
+++ src/pages/BlogsPage.tsx
@@ -453,7 +453,7 @@ export default function BlogsPage() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-50  via-white to-purple-50  ">
-      <div className="container mx-auto px-2 ">
+      <div className="container mx-auto px-6 ">
 
 
         {/* Search and Filters */}
